372bae90c72e9098e2d771d66a7d053dbd109787
Animinikethan <animinikethan12@gmail.com>
2026-07-04 15:11:33 +0530

    Delete database directory

--- database/seed.sql
+++
@@ -1,4 +0,0 @@
-USE darshanease;
-
-INSERT INTO temples (name, location, facilities, access_info) VALUES
-('Sri Venkateswara Temple', 'Tirupati, Andhra Pradesh', 'Queue complex, drinking water, cloak room', 'Accessible by road and rail');
